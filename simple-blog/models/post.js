423d0430bdd1883df85b600d4154957595b79d93
sincerefly <sincerefly@163.com>
2015-03-02 11:02:11 +0800

    finish chapter-4 article.ejs

--- simple-blog/models/post.js
+++ simple-blog/models/post.js
@@ -54,7 +54,7 @@ Post.prototype.save = function(callback) {
 };
 
 //读取文章及其相关信息
-Post.get = function(name, callback) {
+Post.getAll = function(name, callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
@@ -82,6 +82,37 @@ Post.get = function(name, callback) {
           doc.post = markdown.toHTML(doc.post);
         });
         callback(null, docs);//成功！以数组形式返回查询的结果
+      });
+    });
+  });
+};
+
+//获取一篇文章
+Post.getOne = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、发表日期及文章名进行查询
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function (err, doc) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        //解析 markdown 为 html
+        doc.post = markdown.toHTML(doc.post);
+        callback(null, doc);//返回查询的一篇文章
       });
     });
   });
