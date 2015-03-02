f70f6ef501ab4cf30de90dd14c40c07307355b07
sincerefly <sincerefly@163.com>
2015-03-02 23:29:05 +0800

    finish chapter-5 edit && remove article

--- simple-blog/models/post.js
+++ simple-blog/models/post.js
@@ -117,3 +117,94 @@ Post.getOne = function(name, day, title, callback) {
     });
   });
 };
+
+//返回原始发表的内容（markdown 格式）
+Post.edit = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、发表日期及文章名进行查询
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function (err, doc) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, doc);//返回查询的一篇文章（markdown 格式）
+      });
+    });
+  });
+};
+
+//更新一篇文章及其相关信息
+Post.update = function(name, day, title, post, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //更新文章内容
+      collection.update({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, {
+        $set: {post: post}
+      }, function (err) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
+    });
+  });
+};
+
+//删除一篇文章
+Post.remove = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、日期和标题查找并删除一篇文章
+      collection.remove({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, {
+        w: 1
+      }, function (err) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
+    });
+  });
+};
--- simple-blog/routes/index.js
+++ simple-blog/routes/index.js
@@ -191,6 +191,51 @@ router.get('/u/:name/:day/:title', function (req, res) {
   });
 });
 
+router.get('/edit/:name/:day/:title', checkLogin);
+router.get('/edit/:name/:day/:title', function (req, res) {
+  var currentUser = req.session.user;
+  Post.edit(currentUser.name, req.params.day, req.params.title, function (err, post) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect('back');
+    }
+    res.render('edit', {
+      title: '编辑',
+      post: post,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+
+router.post('/edit/:name/:day/:title', checkLogin);
+router.post('/edit/:name/:day/:title', function (req, res) {
+  var currentUser = req.session.user;
+  Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function (err) {
+    var url = encodeURI('/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title);
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect(url);//出错！返回文章页
+    }
+    req.flash('success', '修改成功!');
+    res.redirect(url);//成功！返回文章页
+  });
+});
+
+router.get('/remove/:name/:day/:title', checkLogin);
+router.get('/remove/:name/:day/:title', function (req, res) {
+  var currentUser = req.session.user;
+  Post.remove(currentUser.name, req.params.day, req.params.title, function (err) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect('back');
+    }
+    req.flash('success', '删除成功!');
+    res.redirect('/');
+  });
+});
+
 function checkLogin(req, res, next) {
   if (!req.session.user) {
     req.flash('error', '未登录!');
