2462f899a231f146cc3d555ef24a9c5f3d48705d
sincerefly <sincerefly@163.com>
2015-03-03 21:46:50 +0800

    finish chapter-7 paging

--- simple-blog/models/post.js
+++ simple-blog/models/post.js
@@ -88,6 +88,47 @@ Post.getAll = function(name, callback) {
   });
 };
 
+//一次获取十篇文章
+Post.getTen = function(name, page, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      var query = {};
+      if (name) {
+        query.name = name;
+      }
+      //使用 count 返回特定查询的文档数 total
+      collection.count(query, function (err, total) {
+        //根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
+        collection.find(query, {
+          skip: (page - 1)*10,
+          limit: 10
+        }).sort({
+          time: -1
+        }).toArray(function (err, docs) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          //解析 markdown 为 html
+          docs.forEach(function (doc) {
+            doc.post = markdown.toHTML(doc.post);
+          });  
+          callback(null, docs, total);
+        });
+      });
+    });
+  });
+};
+
 //获取一篇文章
 Post.getOne = function(name, day, title, callback) {
   //打开数据库
--- simple-blog/routes/index.js
+++ simple-blog/routes/index.js
@@ -6,6 +6,7 @@ var Post = require('../models/post.js');
 var Comment = require('../models/comment.js');
 
 /* GET home page. */
+/*
 router.get('/', function (req, res) {
   Post.getAll(null, function (err, posts) {
     if (err) {
@@ -15,6 +16,28 @@ router.get('/', function (req, res) {
       title: '主页',
       user: req.session.user,
       posts: posts,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+*/
+
+router.get('/', function (req, res) {
+  //判断是否是第一页，并把请求的页数转换成 number 类型
+  var page = req.query.p ? parseInt(req.query.p) : 1;
+  //查询并返回第 page 页的 10 篇文章
+  Post.getTen(null, page, function (err, posts, total) {
+    if (err) {
+      posts = [];
+    } 
+    res.render('index', {
+      title: '主页',
+      posts: posts,
+      page: page,
+      isFirstPage: (page - 1) == 0,
+      isLastPage: ((page - 1) * 10 + posts.length) == total,
+      user: req.session.user,
       success: req.flash('success').toString(),
       error: req.flash('error').toString()
     });
@@ -153,14 +176,15 @@ router.post('/upload', function(req, res) {
 });
 
 router.get('/u/:name', function (req, res) {
+  var page = req.query.p ? parseInt(req.query.p) : 1;
   //检查用户是否存在
   User.get(req.params.name, function (err, user) {
     if (!user) {
       req.flash('error', '用户不存在!'); 
-      return res.redirect('/');//用户不存在则跳转到主页
+      return res.redirect('/');
     }
-    //查询并返回该用户的所有文章
-    Post.getAll(user.name, function (err, posts) {
+    //查询并返回该用户第 page 页的 10 篇文章
+    Post.getTen(user.name, page, function (err, posts, total) {
       if (err) {
         req.flash('error', err); 
         return res.redirect('/');
@@ -168,9 +192,12 @@ router.get('/u/:name', function (req, res) {
       res.render('user', {
         title: user.name,
         posts: posts,
-        user : req.session.user,
-        success : req.flash('success').toString(),
-        error : req.flash('error').toString()
+        page: page,
+        isFirstPage: (page - 1) == 0,
+        isLastPage: ((page - 1) * 10 + posts.length) == total,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
       });
     });
   }); 
