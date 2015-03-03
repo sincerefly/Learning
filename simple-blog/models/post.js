9e090dc91ebf39e40b5a2b0d6101c92acece742f
sincerefly <sincerefly@163.com>
2015-03-03 10:37:08 +0800

    finish chapter-6 comment

--- simple-blog/models/post.js
+++ simple-blog/models/post.js
@@ -26,7 +26,8 @@ Post.prototype.save = function(callback) {
       name: this.name,
       time: time,
       title: this.title,
-      post: this.post
+      post: this.post,
+      comments: []
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -111,7 +112,12 @@ Post.getOne = function(name, day, title, callback) {
           return callback(err);
         }
         //解析 markdown 为 html
-        doc.post = markdown.toHTML(doc.post);
+        if (doc) {
+          doc.post = markdown.toHTML(doc.post);
+          doc.comments.forEach(function (comment) {
+            comment.content = markdown.toHTML(comment.content);
+          });
+        }
         callback(null, doc);//返回查询的一篇文章
       });
     });
--- simple-blog/routes/index.js
+++ simple-blog/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
+var Comment = require('../models/comment.js');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -188,6 +189,28 @@ router.get('/u/:name/:day/:title', function (req, res) {
       success: req.flash('success').toString(),
       error: req.flash('error').toString()
     });
+  });
+});
+
+router.post('/u/:name/:day/:title', function (req, res) {
+  var date = new Date(),
+      time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
+             date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+  var comment = {
+      name: req.body.name,
+      email: req.body.email,
+      website: req.body.website,
+      time: time,
+      content: req.body.content
+  };
+  var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+  newComment.save(function (err) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect('back');
+    }
+    req.flash('success', '留言成功!');
+    res.redirect('back');
   });
 });
 
