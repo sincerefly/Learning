d3eaa98b53fc8d309f61c13cfe3731a6c1396427
sincerefly <sincerefly@163.com>
2015-03-06 16:36:40 +0800

    finish chapter-11 search & chapter-12 links

--- simple-blog/models/post.js
+++ simple-blog/models/post.js
@@ -357,3 +357,34 @@ Post.getTag = function(tag, callback) {
     });
   });
 };
+
+//返回通过标题关键字查询的所有文章信息
+Post.search = function(keyword, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      var pattern = new RegExp(keyword, "i");
+      collection.find({
+        "title": pattern
+      }, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+         return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
--- simple-blog/routes/index.js
+++ simple-blog/routes/index.js
@@ -224,6 +224,31 @@ router.get('/tags/:tag', function (req, res) {
   });
 });
 
+router.get('/links', function (req, res) {
+  res.render('links', {
+    title: '友情链接',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
+});
+
+router.get('/search', function (req, res) {
+  Post.search(req.query.keyword, function (err, posts) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect('/');
+    }
+    res.render('search', {
+      title: "SEARCH:" + req.query.keyword,
+      posts: posts,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+
 router.get('/u/:name', function (req, res) {
   var page = req.query.p ? parseInt(req.query.p) : 1;
   //检查用户是否存在
