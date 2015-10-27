01dd3713a5ea62ac2d4b2816dc83cad25f28e69f
sincerefly <sincerefly@163.com>
2015-10-27 17:34:56 +0800

    Finished 'create new' access API

--- ant-test/js/my.js
+++ ant-test/js/my.js
@@ -11,11 +11,31 @@ $(function() {
   });
 });
 
+/*
 $(function() {
   $(document).on("click", '#create', function() {
     alert("hello world!");
   });
 });
+*/
 
-
+$(function () {
+  $(document).on("click", '#create', function () {
+    alert($('input[name=title]').val());
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:3000/new',
+      data: {
+        title: $('input[name=title]').val(),
+        date: $('input[name=date]').val(),
+        filename: $('input[name=filename]').val(),
+        tags: $('input[name=tags]').val(),
+        content: $('textarea[name=content]').val()
+      },
+      success: function (res, status, xhr) {
+        alert('success!: ' + res.title);
+      }
+    })
+  })
+})
 
