b458ab08cc7ee22ad500e7ccc5fe9bc0bca2cc35
sincerefly <sincerefly@163.com>
2015-10-28 16:55:19 +0800

    Add list hexo_settings

--- ant-test/js/my.js
+++ ant-test/js/my.js
@@ -1,24 +1,42 @@
+/* 侧边栏响应 */
+
+// 新建文章
 $(function() {
   $(document).on('click', '#new', function() {
     $('.main-container').load("tiles/new.html");
   });
 });
 
-
+// 文章列表
 $(function() {
   $(document).on('click', '#list', function() {
     $('.main-container').load("tiles/list.html");
+    // 载入列表数据
+    $.getJSON("http://127.0.0.1:3000/list", function (json, status) {
+      $("#getlist").html("");
+      $.each(json['data'], function (i, item) {
+        $("#artlist").append(
+          "<li>" + item.title + "</li>"
+        );
+      });
+    });
   });
 });
 
-/*
-$(function() {
-  $(document).on("click", '#create', function() {
-    alert("hello world!");
+// Hexo设置
+$(function () {
+  $(document).on('click', '#hexo_settings', function() {
+    $('.main-container').load("tiles/hexo_settings.html");
+    // 载入Hexo配置文件数据
+    $.getJSON("http://127.0.0.1:3000/hexo_settings", function (json, status) {
+      $("#settings").append(
+        json['data']
+      );
+    });
   });
 });
-*/
 
+/* 新建文章 */
 $(function () {
   $(document).on("click", '#create', function () {
     alert($('input[name=title]').val());
@@ -39,3 +57,17 @@ $(function () {
   })
 })
 
+/*
+$(function () {
+  $(document).on("onload", "#getlist", function () {
+    $.getJSON("http://127.0.0.1:3000/list", function (json, status) {
+      $("#getlist").html("");
+      $.each(json['data'], function (i, item) {
+        $("#artlist").append(
+          "<li>" + item.title + "</li>"
+        );
+      });
+    });
+  });
+})
+*/
