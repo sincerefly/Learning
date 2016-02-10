cf1496405d7dac691474c6614fdf518e3d83db61
sincerefly <sincerefly@163.com>
2016-02-10 21:40:19 +0800

    add some canvas demo

--- RxJS/2.js
+++ RxJS/2.js
@@ -2,9 +2,9 @@ console.clear();
 
 var source = [1, 2, 3, 4, 5];
 
-var source = Rx.Observable.fromArray([0, 1, 2, 3, 4, 5]);
+//var source = Rx.Observable.fromArray([0, 1, 2, 3, 4, 5]);
 
-var source = Rx.Observable.interval(500).take(6);
+//var source = Rx.Observable.interval(500).take(10);
 
 source.filter(x => x % 2 === 1)
   .map(x => x + '!')
